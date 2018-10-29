ca529247d1be5d2dae8d5d23a984596498062408
Your Name <you@example.com>
2018-10-29 19:23:00 -0400

    greet new users and alert others

--- server/server.js
+++ server/server.js
@@ -14,24 +14,31 @@ console.log(__dirname + '/../public/');
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newEmail', {
-        from: 'mike@example.com',
-        text: 'Hey, what is going on',
-        createdAt: 123
-    });
-
     socket.emit('newMessage', {
-        from: 'server@server.com',
-        text: 'Yes, you are the best',
-        createdAt: 1000
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
     });
 
-    socket.on('createEmail', (newEmail) => {
-        console.log('createEmail', newEmail);
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
-    socket.on('createMessage', (newMessage) => {
-        console.log('createMessage', newMessage);
+    socket.on('newMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // })
     });
 
     socket.on('disconnect', (socket) => {
