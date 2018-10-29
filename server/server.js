a5a381eadd5b3938da4c61c20689b8d5675e493f
Your Name <you@example.com>
2018-10-29 18:52:05 -0400

    Added emitting feature

--- server/server.js
+++ server/server.js
@@ -14,9 +14,29 @@ console.log(__dirname + '/../public/');
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newEmail', {
+        from: 'mike@example.com',
+        text: 'Hey, what is going on',
+        createdAt: 123
+    });
+
+    socket.emit('newMessage', {
+        from: 'server@server.com',
+        text: 'Yes, you are the best',
+        createdAt: 1000
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    });
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
+    });
+
     socket.on('disconnect', (socket) => {
         console.log('User was disconnected');
-    })
+    });
 });
 
 
