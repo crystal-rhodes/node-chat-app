6b0f291bd54eb799f04f05bc765c30e46a98002c
Your Name <you@example.com>
2018-10-29 17:52:26 -0400

    updated style and index.html

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const express = require('express');
 
-const app = express();
+let app = express();
 const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, '/../public/');
 
 console.log(__dirname + '/../public/');
 console.log(publicPath);
+
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
     console.log(`server is up on port ${port}`);
