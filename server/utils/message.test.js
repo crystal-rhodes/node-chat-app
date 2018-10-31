7f813c7b6124ac21b06d35d8779a7b9dea4936da
Your Name <you@example.com>
2018-10-31 18:23:18 -0400

    added geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,6 +9,20 @@ describe('generateMessage', () => {
         const res = generateMessage(from, text);
 
         expect(res).toInclude({from, text});
+        expect(res.createdAt).toBeA('number');
+    });
+}); 
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const from = 'Man Vu';
+        const latitude = 0;
+        const longitude = 0;
+        const url = 'https://www.google.com/maps?q=0,0'
+        
+        const res = generateLocationMessage(from, latitude, longitude);
+
+        expect(res).toInclude({from, url});
         expect(res.createdAt).toBeA('number');
     });
 });
