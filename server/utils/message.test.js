3f4acec3eb1af4bc78ae12ecbdf8ec1cecb580ff
Vu Minh Man <crystalrhodes1997@gmail.com>
2019-01-31 19:50:49 -0500

    Added chat room

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -3,26 +3,25 @@ var expect = require('expect');
 var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
-        const from = 'Man Vu';
-        const text = 'I am the best';
-        const res = generateMessage(from, text);
+  it('should generate correct message object', () => {
+    var from = 'Jen';
+    var text = 'Some message';
+    var message = generateMessage(from, text);
 
-        expect(res).toInclude({from, text});
-        expect(res.createdAt).toBeA('number');
-    });
-}); 
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
 
 describe('generateLocationMessage', () => {
-    it('should generate correct location object', () => {
-        const from = 'Man Vu';
-        const latitude = 0;
-        const longitude = 0;
-        const url = 'https://www.google.com/maps?q=0,0'
-        
-        const res = generateLocationMessage(from, latitude, longitude);
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
 
-        expect(res).toInclude({from, url});
-        expect(res.createdAt).toBeA('number');
-    });
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
 });
